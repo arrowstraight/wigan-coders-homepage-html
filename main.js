413ad88729f9fbed599a53bdba7435051ce2a644
Ryan Brockley <rybrockers@yahoo.com>
2019-12-01 16:31:39 +0000

    Added usp bar

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-var text = ["Welcome to Wigan Coders", "Hi", "Sup dude"];
+var text = ["Welcome to Wigan Coders", "Learn HTML, CSS and JavaScript", "Start your journey into Web Development today!"];
 var counter = 0;
-var elem = document.getElementById("changeText");
+var element = document.getElementById("changeText");
 var inst = setInterval(change, 5000);
 
 function change() {
-  elem.innerHTML = text[counter];
+  element.innerHTML = text[counter];
   counter++;
   if (counter >= text.length) {
     counter = 0;
     // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
   }
-
+  element.style.textAlign = 'centre';
+  element.style.opacity = 0.9;
+  element.classList.toggle('disabled');
 }
