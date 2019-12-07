52ce1500781febdc0c92e291fc87ebe3475732fc
Ryan Brockley <rybrockers@yahoo.com>
2019-12-07 15:41:34 +0000

    added animated text

--- public/main.js
+++ public/main.js
@@ -117,3 +117,25 @@ var x = setInterval(function() {
 // const today = date.dateFns.format(now, "dddd Do MMMM YYYY");
 
 // console.log(today);
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 10000
+  });
