b4e68c9c8420d97fe60c7c7441dcb26dcebc2f67
Ryan Brockley <rybrockers@yahoo.com>
2019-12-07 15:52:28 +0000

    added animated text

--- public/main.js
+++ public/main.js
@@ -139,3 +139,25 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 10000
   });
+
+  // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml7 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml7 .letter',
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 750,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml7',
+    opacity: 0,
+    duration: 3000,
+    easing: "easeOutExpo",
+    delay: 11000
+  });
