b12a576a34eea713fd0b1159809e6fb25c65fe62
Ryan Brockley <rybrockers@yahoo.com>
2019-12-07 15:21:53 +0000

    set popup to every 1 min

--- public/main.js
+++ public/main.js
@@ -48,7 +48,7 @@ function showDivs(n) {
 
 var popupTimer = setInterval(function () {
 popup.style.display = 'block';
-}, 40000);
+}, 60000);
 
 close.addEventListener('click', () => {
 popup.style.display = 'none';
