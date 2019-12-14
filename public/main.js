745d9277fe9e4369c5b131c8983de30d50925554
Ryan Brockley <rybrockers@yahoo.com>
2019-12-14 10:47:30 +0000

    Changed slider button styles

--- public/main.js
+++ public/main.js
@@ -54,6 +54,38 @@ close.addEventListener('click', () => {
 popup.style.display = 'none';
 });
 
+
+// countdown timer
+// Set the date we're counting down to
+var countDownDate = new Date("dec 21, 2019 10:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="date"
+  document.getElementById("date").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("date").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
 // clock
 
 // const clock = document.querySelector('.clock');
@@ -76,39 +108,6 @@ popup.style.display = 'none';
 
 // };
 // setInterval(tick, 1000);
-
-// countdown timer
-
-
-// Set the date we're counting down to
-var countDownDate = new Date("dec 14, 2019 10:30:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("date").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
 
 // navbar date
 
