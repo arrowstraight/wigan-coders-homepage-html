732f049ae77196ba4e2b72597ca5b5100f208b54
Ryan Brockley <rybrockers@yahoo.com>
2020-01-04 10:56:56 +0000

    re set counter popup

--- public/main.js
+++ public/main.js
@@ -57,7 +57,7 @@ popup.style.display = 'none';
 
 // countdown timer
 // Set the date we're counting down to
-var countDownDate = new Date("jan 04, 2020 10:30:00").getTime();
+var countDownDate = new Date("jan 11, 2020 10:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -68,7 +68,7 @@ var x = setInterval(function() {
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
+  // Time calcs for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
